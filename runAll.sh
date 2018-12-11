9049130fb947c754e32206b6782abe6363e9493c
m2bi Inka LEROY <leroy.inka@yahoo.fr>
2018-12-11 18:16:46 +0100

    script bash

---
+++ runAll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#####Repositories creation#####
+rm -Rf Query DSSP suivi
+mkdir Query DSSP suivi
+
+#####PDB queries will be relocalized in Query repository#####
+
+for query in "$1"* ; do
+    cp $query ./Query
+done
+
+######Secondary structure assignation with DSSP#####
+for f in Query/* ; do
+	pdb=`echo $f | cut -d/ -f2`
+	name=`echo ${f##*/} | cut -d. -f1`
+	./dssp-2.0.4-linux-amd64 -i ./$f -o DSSP/$name.out &>> suivi/tmp.log
+	python3 ./parse_dssp.py DSSP/$name.out
+done
+
