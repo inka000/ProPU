0b130e08c03c5d2adb01bcb8cf4a97f543084477
m2bi Inka LEROY <leroy.inka@yahoo.fr>
2018-12-17 18:30:30 +0100

    maj runAll.sh

--- runAll.sh
+++ runAll.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #####Repositories creation#####
-rm -Rf Query DSSP suivi
-mkdir Query DSSP suivi
+rm -Rf Query DSSP resultPI suivi
+mkdir Query DSSP resultPI suivi
 
 #####PDB queries will be relocalized in Query repository#####
 
@@ -15,6 +15,7 @@ for f in Query/* ; do
 	pdb=`echo $f | cut -d/ -f2`
 	name=`echo ${f##*/} | cut -d. -f1`
 	./dssp-2.0.4-linux-amd64 -i ./$f -o DSSP/$name.out &>> suivi/tmp.log
-	#python3 ./parse_dssp.py DSSP/$name.out
+	echo 'Query ='$name
+	python3 info.py $f
 done
 
