b9ff9795fbf2cf66f9406d6fd6dfab1dee03516b
m2bi Inka LEROY <leroy.inka@yahoo.fr>
2018-12-18 19:34:06 +0100

    analyses maj

--- analyses.py
+++ analyses.py
@@ -18,11 +18,11 @@ def read_info(namefile) :
     with open(namefile, "r") as file :
         line = file.readline()
         for line in file :
-            list_begin.append(line.split("\t")[0])
-            list_size.append(line.split("\t")[1])
-            list_PI.append(line.split("\t")[2])
-            list_sigma.append(line.split("\t")[3])
-            list_k.append(line.split("\t")[4])
+            list_begin.append(float(line.split("\t")[0]))
+            list_size.append(float(line.split("\t")[1]))
+            list_PI.append(float(line.split("\t")[2]))
+            list_sigma.append(float(line.split("\t")[3]))
+            list_k.append(float(line.split("\t")[4]))
     return(list_begin, list_size, list_PI, list_sigma, list_k)
 
 
@@ -49,8 +49,12 @@ def main() :
             list_temp_size.append(float(list_size[i]))
 
 
-    plt.plot(list_temp_size, list_temp_PI, 'ro')
-    plt.axis([min(list_temp_size), max(list_temp_size), min(list_temp_PI), max(list_temp_PI)])
+    #plt.plot(list_temp_size, list_temp_PI, 'ro')
+    #plt.axis([min(list_temp_size), max(list_temp_size), min(list_temp_PI), max(list_temp_PI)])
+    #plt.show()
+
+    plt.plot(list_size, list_k, 'ro')
+    plt.axis([min(list_size), max(list_size), min(list_k), max(list_k)])
     plt.show()
 
 
